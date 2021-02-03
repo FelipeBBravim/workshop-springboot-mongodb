f5b32cdd5a9369137bd39b50271e2ee40d87e61a
FelipeBBravim <felipe.bravim@gmail.com>
2021-02-03 10:28:04 -0300

    Obtendo um post por idC

---
+++ src/main/java/com/educandoweb/workshopmongodb/services/PostService.java
@@ -0,0 +1,22 @@
+package com.educandoweb.workshopmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.workshopmongodb.domain.Post;
+import com.educandoweb.workshopmongodb.repository.PostRepository;
+import com.educandoweb.workshopmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo; 
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
